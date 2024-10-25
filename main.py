64b2c07c955f86634a787db24817648233cca117
Akshay-ingle <akshy.nilesh.ingle@gmail.com>
2024-10-25 19:21:09 +0530

    first activity

---
+++ main.py
@@ -0,0 +1,11 @@
+#printing helloworld
+print("hello world")
+
+#printing my name
+print("akshay")
+
+#printing a fun fact  about my self
+print("I like to code")
+
+#we will printing sum of 3 and 5
+print("sum 3 and 5=",3+5)
